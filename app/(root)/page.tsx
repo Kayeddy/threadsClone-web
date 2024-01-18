ca342bc07236eb53d952e841d337c2c570a98263
Edward Anthony Escudero Bowie <57167218+Kayeddy@users.noreply.github.com>
2024-01-17 22:32:09 -0500

    Updated main threads page UI
    Fixed bug that broke the UI of the threads page when there were too many threads posted.

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -7,14 +7,16 @@ export default async function Home() {
   const currentLoggedInUser = await currentUser();
 
   return (
-    <div className="h-screen">
-      <UserButton afterSignOutUrl="/" />
-      <h1 className="head-text text-left">Home</h1>
+    <div className="overflow-hidden flex flex-col relative">
+      <section className="flex flex-row gap-4 items-center justify-start ml-2">
+        <UserButton afterSignOutUrl="/" />
+        <h1 className="head-text">Home</h1>
+      </section>
       <section className="mt-9 flex flex-col gap-10">
         {threads.length === 0 ? (
           <p className="no-result">No threads available at this moment</p>
         ) : (
-          <>
+          <div className="h-auto overflow-x-hidden overflow-y-auto relative flex flex-col gap-10 py-10">
             {threads.map((thread) => {
               const threadCardProps = {
                 threadId: thread._id,
@@ -31,7 +33,7 @@ export default async function Home() {
 
               return <ThreadCard key={thread._id} {...threadCardProps} />;
             })}
-          </>
+          </div>
         )}
       </section>
     </div>
