9c0c4c47de74f4e9f52f51be8ecb6be97ad59674
Edward Anthony Escudero Bowie <57167218+Kayeddy@users.noreply.github.com>
2024-01-31 12:22:16 -0500

    updated Community actions

--- lib/actions/community.actions.ts
+++ lib/actions/community.actions.ts
@@ -181,36 +181,36 @@ export async function addMemberToCommunity(
     connectToDB();
 
     // Find the community by its unique id
-    const community = await Community.findOne({ id: communityId });
+    const targetCommunity = await Community.findOne({ id: communityId });
 
-    if (!community) {
+    if (!targetCommunity) {
       throw new Error("Community not found");
     }
 
     // Find the user by their unique id
-    const user = await User.findOne({ id: memberId });
+    const targetUser = await User.findOne({ userId: memberId });
 
-    if (!user) {
+    if (!targetUser) {
       throw new Error("User not found");
     }
 
     // Check if the user is already a member of the community
-    if (community.members.includes(user._id)) {
+    if (targetCommunity.members.includes(targetUser._id)) {
       throw new Error("User is already a member of the community");
     }
 
     // Add the user's _id to the members array in the community
-    community.members.push(user._id);
-    await community.save();
+    targetCommunity.members.push(targetCommunity._id);
+    await targetCommunity.save();
 
     // Add the community's _id to the communities array in the user
-    user.communities.push(community._id);
-    await user.save();
+    targetUser.communities.push(targetCommunity._id);
+    await targetUser.save();
 
-    return community;
+    return targetCommunity;
   } catch (error) {
     // Handle any errors
-    console.error("Error adding member to community:", error);
+    console.error("Error adding member to community. Error details => ", error);
     throw error;
   }
 }
